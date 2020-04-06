8698c3d5bd4a13466accfe883866e543c6d22d36
Christopher Wycoff <christopherwycoff@Christophers-MacBook-Pro-7.local>
2020-04-06 15:51:39 -0700

    lab2

---
+++ labs/lab_2/lab2-skeleton.c
@@ -0,0 +1,45 @@
+/*
+* Description: <write a brief description of your lab>
+*
+* Author: Christopher Wycoff 951078512
+*
+* Date: <today's date>
+*
+* Notes: 
+* 1. <add notes we should consider when grading>
+*/
+
+/*-------------------------Preprocessor Directives---------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*---------------------------------------------------------------------------*/
+
+/*-----------------------------Program Main----------------------------------*/
+int main() {
+	setbuf(stdout, NULL);
+	
+	/* Main Function Variables */
+	//line_buffer = (char *)malloc()
+	
+	/* Allocate memory for the input buffer. */
+	
+	/*main run loop*/
+	while(1){
+		printf(">>>\n");
+		//getline();
+		/* Print >>> then get the input string */
+
+		/* Tokenize the input string */
+
+		/* Display each token */
+		
+		/* If the user entered <exit> then exit the loop */
+		exit(1);
+	}
+	
+	/*Free the allocated memory*/
+
+	return 1;
+}
+/*-----------------------------Program End-----------------------------------*/
