f84a6e1f53fde3be323c32d13567b70c50b9d135
Christopher Wycoff <christopherwycoff@Christophers-MacBook-Pro-7.local>
2020-05-10 12:37:16 -0700

    added turn in directory

---
+++ project2_turn_in/header.h
@@ -0,0 +1,26 @@
+// header.h
+
+#include <stdio.h>
+#include <signal.h>
+
+
+int dummy_fucntion(){
+	printf("I am dumb\n");
+
+	return 0;
+}
+
+int exit_function(int status, char * line_buffer, char ***the_args, char** the_programs, 
+	int * arguments_per_program, int number_of_programs, char*** copy_of_args);
+
+void signal_handler(int sig, siginfo_t * siginfo, void *context);
+
+void display_status(long tgid);
+
+void get_exec_time(long tgid);
+
+void display_memory(long tgid);
+
+void display_io_read(long tgid);
+
+void display_io_write(long tgid);
