ec7d3b8631ef5c38223da0fa89b7f76f807c3ceb
Christopher Wycoff <christopherwycoff@Christophers-MacBook-Pro-7.local>
2020-05-10 09:32:53 -0700

    finishign some stuff up

--- project2_editable/header.h
+++ project2_editable/header.h
@@ -1,6 +1,7 @@
 // header.h
 
 #include <stdio.h>
+#include <signal.h>
 
 
 int dummy_fucntion(){
@@ -8,3 +9,18 @@ int dummy_fucntion(){
 
 	return 0;
 }
+
+int exit_function(int status, char * line_buffer, char ***the_args, char** the_programs, 
+	int * arguments_per_program, int number_of_programs, char*** copy_of_args);
+
+void signal_handler(int sig, siginfo_t * siginfo, void *context);
+
+void display_status(long tgid);
+
+void get_exec_time(long tgid);
+
+void display_memory(long tgid);
+
+void display_io_read(long tgid);
+
+void display_io_write(long tgid);
